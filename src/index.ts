d5d8c420a2bc12c3bfd9a40a63e1d5beeee25d52
Emile Rolley <emile.rolley@tuta.io>
2025-01-27 17:19:30 +0100

    feat: add extractLocalisationFromAST

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import _communes from "@etalab/decoupage-administratif/data/communes.json";
+import _departements from "@etalab/decoupage-administratif/data/departements.json";
+import _regions from "@etalab/decoupage-administratif/data/regions.json";
 import _epci from "@etalab/decoupage-administratif/data/epci.json";
-import { Situation } from "../publicodes-build/";
+import { RuleName, Situation } from "../publicodes-build/";
+import { ASTNode, reduceAST } from "publicodes";
 
 /**
  * NOTE: this type has been inferred from the
@@ -52,15 +55,34 @@ export type EPCI = {
   >;
 };
 
+export type Region = {
+  code: string;
+  chefLieu: string;
+  nom: string;
+  typeLiaison?: 0 | 1 | 2 | 3 | 4;
+  zone: "metro" | "drom" | "com";
+};
+
+export type Departement = {
+  code: string;
+  region: Region["code"];
+  chefLieu: string;
+  nom: string;
+  typeLiaison?: 0 | 1 | 2 | 3 | 4 | 5;
+  zone: "metro" | "dom" | "com";
+};
+
 const communes = _communes as Commune[];
 const epci = _epci as EPCI[];
+const departements = _departements as Departement[];
+const regions = _regions as Region[];
 
 /** Associate each commune INSEE code to its EPCI SIREN code.
  *
  *  PERF: should we do this at build time?
  */
 const epci_by_communes = Object.fromEntries(
-  epci.flatMap((epci) => epci.membres.map(({ code }) => [code, epci.code])),
+  epci.flatMap((epci) => epci.membres.map(({ code }) => [code, epci]))
 );
 
 /**
@@ -74,27 +96,178 @@ const epci_by_communes = Object.fromEntries(
  * console.log(getSituationFromCodeINSEE("38185"));
  * // Output:
  * {
- *   "localisation . code insee": "'38185'",    // Grenoble
+ *   "localisation . code commune": "'38185'",  // Grenoble
  *   "localisation . code epci": "'200040715'", // Grenoble-Alpes-Métropole
  *   "localisation . code département": "'38'", // Isère
  *   "localisation . code région": "'84'"       // Auvergne-Rhône-Alpes
  * }
  * ```
  */
-export function getSituationFromCodeINSEE(
-  code: string,
+export function getFullSituationFromCommune(
+  code: string
 ): Omit<Situation, "localisation"> | undefined {
   const commune = communes.find((c) => c.code === code);
   if (commune) {
     return {
-      "localisation . code insee": fmt(code),
-      "localisation . code epci": fmt(epci_by_communes[code]),
-      "localisation . code département": fmt(commune?.departement),
-      "localisation . code région": fmt(commune?.region),
+      "commune . code": fmt(code),
+      "epci . code": fmt(epci_by_communes[code].code),
+      "epci . nom": fmt(epci_by_communes[code].nom),
+      "département . code": fmt(commune?.departement),
+      "région . code": fmt(commune?.region),
     };
   }
 }
 
 function fmt(v: string | undefined): `'${string}'` {
   return `'${v ?? ""}'`;
+}
+
+export type Localisation = {
+  type: "région" | "département" | "epci" | "commune";
+  valeur: string;
+  code: string;
+  nom: string;
+};
+
+const LOCALISATION_KINDS: RuleName[] = [
+  "commune . code",
+  "région . code",
+  "département . code",
+  "epci . code",
+  "epci . nom",
+];
+
+export function extractLocalisationFromAST(
+  rule: ASTNode,
+  namespace?: string
+): Localisation[] {
+  const toKind = (dottedName: string): Localisation["type"] | undefined => {
+    if (dottedName.endsWith("commune . code")) {
+      return "commune";
+    }
+    if (dottedName.endsWith("région . code")) {
+      return "région";
+    }
+    if (dottedName.endsWith("département . code")) {
+      return "département";
+    }
+    if (
+      dottedName.endsWith("epci . code") ||
+      dottedName.endsWith("epci . nom")
+    ) {
+      return "epci";
+    }
+  };
+  const _namespace = namespace ? `${namespace} .` : "";
+  const extract = (
+    ref: ASTNode<"reference">,
+    value: ASTNode<"constant">
+  ): Localisation | undefined => {
+    // TODO: should we throw an error if the value is not a string?
+    if (value.type !== "string") {
+      return;
+    }
+    for (let kind of LOCALISATION_KINDS) {
+      if (ref.dottedName === `${_namespace} ${kind}`) {
+        const valeur = value.nodeValue?.toString();
+        const infos = getInfosFor(kind, valeur);
+
+        if (valeur && infos) {
+          return {
+            type: toKind(ref.dottedName)!,
+            valeur,
+            nom: infos.nom,
+            code: infos.code,
+          };
+        }
+      }
+    }
+  };
+
+  return reduceAST(
+    (acc, node) => {
+      if (node.nodeKind === "operation" && node.operationKind === "=") {
+        const ref =
+          node.explanation[0]?.nodeKind === "reference"
+            ? node.explanation[0]
+            : node.explanation[1].nodeKind === "reference"
+            ? node.explanation[1]
+            : undefined;
+        const value =
+          node.explanation[0]?.nodeKind === "constant"
+            ? node.explanation[0]
+            : node.explanation[1].nodeKind === "constant"
+            ? node.explanation[1]
+            : undefined;
+        if (ref && value) {
+          const localisation = extract(ref, value);
+          if (
+            localisation &&
+            !acc.find(
+              (l: Localisation) =>
+                l.type === localisation.type && l.valeur === localisation.valeur
+            )
+          ) {
+            acc.push(localisation);
+          }
+          return acc;
+        }
+      }
+    },
+    new Array<Localisation>(),
+    rule
+  );
+  //
+  // if (localisations.length === 0) {
+  //   console.warn(`No localisation found for ${rule.dottedName}`);
+  //   return;
+  // }
+
+  // const normalizedLocalisations = localisations.map((loc) => {
+  //   // In our rule basis we reference EPCI by their name but for iteroperability
+  //   // with third-party systems it is more robust to expose their SIREN code.
+  //   if (loc.kind === "epci") {
+  //     const code = epci.find(({ nom }) => nom === loc.value)?.code;
+  //
+  //     if (!code) {
+  //       console.error(`Bad EPCI code: ${loc.value}`);
+  //       exit(1);
+  //     }
+  //
+  //     return { ...loc, code };
+  //   }
+  //
+  //   return loc;
+  // });
+
+  // FIXME: should handle multiple localisations
+  // return normalizedLocalisations[0];
+}
+
+function getInfosFor(
+  kind: RuleName,
+  value: string | undefined
+): Pick<Localisation, "code" | "nom"> | undefined {
+  if (!value) {
+    return;
+  }
+  switch (kind) {
+    case "commune . code": {
+      return communes.find((c) => c.code === value);
+    }
+    case "epci . nom": {
+      return epci.find((c) => c.nom === value);
+    }
+    case "epci . code": {
+      return epci.find((c) => c.code === value);
+    }
+
+    case "région . code": {
+      return regions.find((r) => r.code === value);
+    }
+
+    case "département . code": {
+      return departements.find((d) => d.code === value);
+    }
+  }
 }
---
+++ test/index.test.ts
@@ -0,0 +1,149 @@
+import { test, expect, describe } from "vitest";
+import {
+  extractLocalisationFromAST,
+  getFullSituationFromCommune,
+} from "../src";
+import { getModelFromSource } from "@publicodes/tools/compilation";
+import Engine from "publicodes";
+
+describe("getFullSituationFromCommune", () => {
+  test("'75056'", () => {
+    expect(getFullSituationFromCommune("75056")).toEqual({
+      "commune . code": "'75056'", // Paris
+      "epci . code": "'200054781'", // Métropole du Grand Paris
+      "epci . nom": "'Métropole du Grand Paris'",
+      "département . code": "'75'", // Paris
+      "région . code": "'11'", // Île-de-France
+    });
+  });
+
+  test("'38185'", () => {
+    expect(getFullSituationFromCommune("38185")).toEqual({
+      "commune . code": "'38185'", // Grenoble
+      "epci . code": "'200040715'", // Grenoble-Alpes-Métropole
+      "epci . nom": "'Grenoble-Alpes-Métropole'",
+      "département . code": "'38'", // Isère
+      "région . code": "'84'", // Auvergne-Rhône-Alpes
+    });
+  });
+
+  test("''", () => {
+    expect(getFullSituationFromCommune("")).toBeUndefined();
+  });
+
+  test("'1034134'", () => {
+    expect(getFullSituationFromCommune("")).toBeUndefined();
+  });
+});
+
+describe.only("extractLocalisationFromAST", () => {
+  const testRules = getModelFromSource("test/data/extract.publicodes");
+  const testEngine = new Engine(testRules);
+
+  test("simple commune code", () => {
+    expect(
+      extractLocalisationFromAST(
+        testEngine.getRule("simple commune code"),
+        "localisation"
+      )
+    ).toEqual([
+      {
+        type: "commune",
+        code: "75101",
+        nom: "Paris 1er Arrondissement",
+        valeur: "75101",
+      },
+    ]);
+  });
+
+  test("simple commune code (inversed comparison)", () => {
+    expect(
+      extractLocalisationFromAST(
+        testEngine.getRule("simple commune code . inversed comparison"),
+        "localisation"
+      )
+    ).toEqual([
+      {
+        type: "commune",
+        code: "75101",
+        nom: "Paris 1er Arrondissement",
+        valeur: "75101",
+      },
+    ]);
+  });
+
+  test("simple epci name", () => {
+    expect(
+      extractLocalisationFromAST(
+        testEngine.getRule("simple epci name"),
+        "localisation"
+      )
+    ).toEqual([
+      {
+        type: "epci",
+        code: "243300316",
+        nom: "Bordeaux Métropole",
+        valeur: "Bordeaux Métropole",
+      },
+    ]);
+  });
+
+  test("multiple localisations", () => {
+    expect(
+      extractLocalisationFromAST(
+        testEngine.getRule("multiple localisations"),
+        "localisation"
+      )
+    ).toEqual([
+      {
+        type: "epci",
+        code: "243300316",
+        nom: "Bordeaux Métropole",
+        valeur: "Bordeaux Métropole",
+      },
+      {
+        type: "commune",
+        code: "75101",
+        nom: "Paris 1er Arrondissement",
+        valeur: "75101",
+      },
+    ]);
+  });
+
+  test("multiple localisations complex", () => {
+    expect(
+      extractLocalisationFromAST(
+        testEngine.getRule("multiple localisations . complex"),
+        "localisation"
+      )
+    ).toEqual([
+      {
+        type: "epci",
+        code: "243300316",
+        nom: "Bordeaux Métropole",
+        valeur: "Bordeaux Métropole",
+      },
+      {
+        type: "commune",
+        code: "75101",
+        nom: "Paris 1er Arrondissement",
+        valeur: "75101",
+      },
+      {
+        type: "département",
+        code: "75",
+        nom: "Paris",
+        valeur: "75",
+      },
+    ]);
+  });
+
+  test("simplified reference name", () => {
+    expect(
+      extractLocalisationFromAST(
+        testEngine.getRule("localisation . simplified"),
+        "localisation"
+      )
+    ).toEqual([{ type: "région", code: "06", nom: "Mayotte", valeur: "06" }]);
+  });
+});
